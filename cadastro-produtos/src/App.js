dfb1a13f3a868b9c6e7f2054b05d8e14fec3bb29
joaojvf <joao.victorf02@hotmail.com>
2020-04-21 13:55:44 -0300

    Criando a tela home

--- cadastro-produtos/src/App.js
+++ cadastro-produtos/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./views/Home";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <div className="container">
+        <Navbar></Navbar>
+        <Home></Home>
+      </div>
+    </>
   );
 }
 
