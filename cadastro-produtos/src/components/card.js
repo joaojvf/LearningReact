c6f68b6df4b98203a518d5a52ca43a8a945974aa
joaojvf <joao.victorf02@hotmail.com>
2020-04-23 17:50:59 -0300

    Criada funcionalidade excluir

---
+++ cadastro-produtos/src/components/card.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => (
+    <div className="card">
+        <div className="card-header"></div>
+        <div className="card-body"></div>
+    </div>
+)
